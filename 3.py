b171538f336db899d7d316d9300b2487f9f89016
mamsdeveloper <belk1naalyona@yandex.ru>
2022-02-22 09:37:26 +0500

    3.py solved

---
+++ 3.py
@@ -0,0 +1,21 @@
+"""
+4. Create a Python project to get the current stock price of specified companies.
+
+
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def stock_price(symbol: str = "AAPL") -> str:
+    url = f"https://in.finance.yahoo.com/quote/{symbol}?s={symbol}"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    class_ = "My(6px) Pos(r) smartphone_Mt(6px)"
+    return soup.find("div", class_=class_).find("span").text
+
+
+if __name__ == "__main__":
+    for symbol in "AAPL AMZN IBM GOOG MSFT ORCL ABM A".split():
+        print(f"Current {symbol:<4} stock price is:  {stock_price(symbol):>8}")
